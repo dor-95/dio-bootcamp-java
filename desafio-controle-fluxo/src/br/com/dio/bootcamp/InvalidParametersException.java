4ceb6a0f35cba3984ca8651e9fa2c0456e6b940d
Diego <diego.rocha@edu.unipar.br>
2023-09-04 15:41:37 -0300

    feat: created custom exception

---
+++ desafio-controle-fluxo/src/br/com/dio/bootcamp/InvalidParametersException.java
@@ -0,0 +1,7 @@
+package br.com.dio.bootcamp;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+}
