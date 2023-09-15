789f75e8576e94d9bc4aaecf0365dd767b4bf684
Diego <diego.rocha@edu.unipar.br>
2023-09-15 10:10:19 -0300

    feat: implemented research exercise with SET

---
+++ collections-java-api-2023/src/main/java/map/pesquisa/AgendaContatosTest.java
@@ -0,0 +1,22 @@
+package main.java.map.pesquisa;
+
+import java.util.Set;
+
+public class AgendaContatosTest {
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Rosa", 123456);
+        agendaContatos.adicionarContato("Rosa", 0);
+        agendaContatos.adicionarContato("Rosa D", 1111111);
+        agendaContatos.adicionarContato("Rosa C", 654987);
+        agendaContatos.adicionarContato("Hinata H", 1111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Hinata"));
+
+        System.out.println("Contato atualizado: " + agendaContatos.atualizarNumeroContato("Hinata H", 5555555));
+    }
+}
